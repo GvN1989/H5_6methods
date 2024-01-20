2213f60a71a886ef7badad24623090dca089c5fe
GvN1989 <geesje.noordenburg@gmail.com>
2024-01-20 13:52:42 +0100

    uitwerking voltooid

--- date-methods.js
+++ date-methods.js
@@ -13,8 +13,20 @@ console.log(currentDay);
 // Uren: 16 (als er een vier in de klok zit op het moment van aanroepen)
 // Minuten: 36 (als het 16:36 is op het moment van aanroepen)
 
+const today = new Date();
 
+const hoursToday = today.getHours()
+
+console.log(hoursToday)
+
+const minutesToday = today.getMinutes()
+
+console.log(hoursToday,minutesToday)
 
 /* Opdracht 2 */
 // Format het date-object dat je bij de vorige vraag hebt gemaakt naar een leesbare datum
 // ---- Verwachte uitkomst: Tue Apr 06 2021 (afhankelijk van het moment)
+
+const date = today.toDateString()
+
+console.log(date)
--- string-methods.js
+++ string-methods.js
@@ -13,7 +13,7 @@ console.log(word.length);
 const fruit = "Banaan";
 const vegetable = "Aardappel";
 
-
+console.log(fruit.length > vegetable.length)
 
 /* Opdracht 2 */
 //  Zorg ervoor dat de laatste letter van het woord in fruit en vegetable (hierboven) in de terminal gelogd wordt.
@@ -21,7 +21,13 @@ const vegetable = "Aardappel";
 // ---- Verwachte uitkomst voor fruit: "n"
 // ---- Verwachte uitkomst voor vegetable: "l"
 
+const lastIndexFruit = fruit.length - 1
+const lastIndexVegetable = vegetable.length-1
 
+const lastFruit = fruit.substring(lastIndexFruit)
+const lastVegetable = vegetable.substring(lastIndexVegetable)
+
+console.log (lastFruit,lastVegetable)
 
 /* Opdracht 3 */
 //  Haal de spaties aan het begin en eind van onderstaande zin weg en log dit in de terminal
@@ -29,6 +35,9 @@ const vegetable = "Aardappel";
 
 const userInput = "  De kat krabt de krullen van de trap ";
 
+const trimUserInput = userInput.trim()
+
+console.log(trimUserInput)
 
 
 /* Opdracht 4 */
@@ -37,13 +46,20 @@ const userInput = "  De kat krabt de krullen van de trap ";
 
 const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch, nietwaar?"
 
+const includeword = story.includes("Fantastisch")
 
+console.log(includeword)
 
 /* Opdracht 5 */
 // Knip de vraag van bovenstaande tekst (story) af, zodat alleen het eerste gedeelte overblijft en log dit in de terminal.
 // ---- Verwachte uitkomst: "We vinden de behaalde resultaten werkelijk prachtig."
 
 
+const cutSentence= story.indexOf(".")
+
+const shortSentence= story.substring(0, cutSentence + 1)
+
+console.log(shortSentence)
 
 /* Opdracht 6 */
 // Maak van onderstaand bedrag een Nederlands bedrag door het volgende te doen:
@@ -54,6 +70,9 @@ const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch,
 
 const price = "$35.59";
 
+const euroPrice = (price.replace("$","€").replace(".", "," ))
+
+console.log (euroPrice)
 
 
 
